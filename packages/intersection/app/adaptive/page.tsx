0987442b104ff3ed2a6fd754ed728a303690c9b3
Blake Netzeband <blake.netzeband@gmail.com>
2025-01-17 11:36:05 -0700

    added routes

---
+++ packages/intersection/app/adaptive/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default async function Adaptive() {
+  return (
+    <main>
+      <h1>Adaptive</h1>
+      <Link href="/">Go Home</Link>
+    </main>
+  );
+}
---
+++ packages/intersection/app/fixed-time/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default async function FixedTime() {
+  return (
+    <main>
+      <h1>fixed time</h1>
+      <Link href="/">Go Home</Link>
+    </main>
+  );
+}
